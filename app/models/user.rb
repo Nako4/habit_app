b246dcae7d4fb0f923752caefd75040965b6843b
Nako4 <papnoatelier@gmail.com>
2020-11-24 20:19:09 +0900

    単体テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  with_options presence: true do
+    validates :nickname, length: {maximum: 15 }
+    validates :password, format: {with: PASSWORD_REGEX }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  describe 'ユーザー登録' do
+    # 正常系
+    context 'ユーザー登録がうまくいく時' do
+      it 'ニックネーム、メールアドレス、パスワードがあれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+    # 異常系
+    context 'ユーザー登録ができない時' do
+      it 'ニックネームがない時' do
+        @user.nickname = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
+      it 'メールアドレスがない時' do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it 'メールアドレスが重複する時' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+
+      it 'パスワードがない時' do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank", "Password can't be blank", "Password is invalid")
+      end
+
+    end
+  end
 end
