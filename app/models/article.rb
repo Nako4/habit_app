b0255ec35b1f2b3be0897d0570a30d47a961a610
Nako4 <papnoatelier@gmail.com>
2020-11-26 06:55:45 +0900

    tagテーブル、中間テーブルの作成とバリデーションの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,7 @@ class Article < ApplicationRecord
     validates :title, length: { maximum: 40 }
     validates :output, length: { maximum: 400 }
   end
+
+  has_many :article_tag_relations
+  has_many :tags, through: :article_tag_relations
 end
