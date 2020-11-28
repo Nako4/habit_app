01f60c892dba82124db35f48f6816154e387889b
Nako4 <papnoatelier@gmail.com>
2020-11-28 12:35:21 +0900

    データベースに保存されたactionの表示を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+
+  # def index
+  #   @articles = Article.all.order('created_at DESC')
+  # end
   def show
-    
+    @articles = Article.where(user_id: current_user.id)
   end
 end
