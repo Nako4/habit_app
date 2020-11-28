0083a7a3721d8780eefa1779ee4369f8f57dadae
Nako4 <papnoatelier@gmail.com>
2020-11-28 16:00:17 +0900

    不要な記述を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
-
-  # def index
-  #   @articles = Article.all.order('created_at DESC')
-  # end
   def show
     @articles = Article.where(user_id: current_user.id).order('created_at DESC')
   end
