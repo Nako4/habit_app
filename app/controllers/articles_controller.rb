32bf4a3ef5b8108c730982fad441a72a7047440e
Nako4 <papnoatelier@gmail.com>
2020-11-28 09:25:09 +0900

    rubocopで修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :set_article, only: [:show, :edit]
   # before_action :move_to_index, except: [:index, :show]
 
@@ -39,9 +39,11 @@ class ArticlesController < ApplicationController
 
   private
 
-  #formオブジェクトから保存する値は全てpermitで指定する
+  # formオブジェクトから保存する値は全てpermitで指定する
   def article_params
-    params.require(:article_tag).permit(:title, :output, :action, :user_id, :article_id, :tag_name, :tag_id).merge(user_id: current_user.id)
+    params.require(:article_tag).permit(
+      :title, :output, :action, :user_id, :article_id, :tag_name, :tag_id
+    ).merge(user_id: current_user.id)
   end
 
   def set_article
--- app/models/article_tag.rb
+++ app/models/article_tag.rb
@@ -1,18 +1,15 @@
 class ArticleTag
   include ActiveModel::Model
   attr_accessor :title, :output, :action, :tag_name, :user_id
-  
+
   with_options presence: true do
     validates :title, length: { maximum: 40 }
     validates :output, length: { maximum: 400 }
   end
-
-
 
   def save(current_user)
     article = Article.create(title: title, output: output, action: action, user_id: current_user.id)
     tag = Tag.create(tag_name: tag_name)
     ArticleTagRelation.create(article_id: article.id, tag_id: tag.id)
   end
-
 end
