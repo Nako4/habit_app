8a96363fc0fc5ac9bc2924e6cbee06fa7193a878
Nako4 <papnoatelier@gmail.com>
2020-12-19 10:18:09 +0900

    達成度に応じて背景色を変える機能の実装

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -6,15 +6,43 @@ window.addEventListener('load', function(){
     day.addEventListener("click", () => {
       const dayText = day.innerHTML.trim();
       document.getElementById('date-record').value = dayText
-      const dayPanel = day.parentNode
-      console.log(dayPanel)
       
     });
+
+  
+
+
+
   });
+
+    //達成度のかかれたパネルを取得
+    //パネルの数値を取得
+    //数値に応じてパネルの背景色を変更
+
+  let achievePanels = document.getElementsByClassName("achieve-rate")
+  achievePanels = Array.from(achievePanels);
+  console.log(achievePanels)
+    achievePanels.forEach(function (achievePanel) {
+      const dayPanel = achievePanel.parentNode
+      const achieveValueElement = achievePanel.innerHTML
+      const achieveValue = Number( achieveValueElement.replace(/[^0-9]/g, '') )
+      console.log(achieveValue)
+      if (achieveValue >=80) {
+        dayPanel.style.backgroundColor = "#12DD09";
+      } else if (achieveValue >= 60) {
+        dayPanel.style.backgroundColor = "#2FF626";
+      } else if (achieveValue >= 40) {
+        dayPanel.style.backgroundColor = "#89F983";
+      } else if (achieveValue >= 20) {
+        dayPanel.style.backgroundColor = "#D1FDCF";
+      } 
+
+      
+    })
+
 
   $(function(){
     let countAll = $('input:checkbox').length;
-    console.log(countAll)
     $('input:checkbox').change(function(){
       // let countCheck = $('#action-list input:checkbox:checked').length;
       //idをいれるとなぜか一番上しかカウントされない。
@@ -22,7 +50,7 @@ window.addEventListener('load', function(){
       let countCheck = $('input:checkbox:checked').length;
       let achieveRate = countCheck / countAll　* 100
       $('span.achieve-count').text(achieveRate);
-      const achieveValue = document.getElementById('form-value')
+      // const achieveValue = document.getElementById('form-value')
       $('input.form-value').val(achieveRate);
     }).trigger('change');
   })
