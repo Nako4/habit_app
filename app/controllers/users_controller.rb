5b6b77867d86d5b97b838132a00cbd609fcd6c42
Nako4 <papnoatelier@gmail.com>
2020-12-19 11:18:43 +0900

    rubocopでコード修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
-
   def create
     @article = Article.where(user_id: current_user.id)
-    @habit = Habit.new(habit_params)
-    if @habit.valid?
-      @habit.save
-      return redirect_to users_show_path
-    end
+    @habit = Habit.create(habit_params)
+    redirect_to users_show_path
   end
 
   def show
-    @articles = Article.where(user_id: current_user.id).order('created_at DESC') 
+    @articles = Article.where(user_id: current_user.id).order('created_at DESC')
     @habits = Habit.where(user_id: current_user.id)
   end
 
@@ -19,5 +15,4 @@ class UsersController < ApplicationController
   def habit_params
     params.permit(:date, :achieve).merge(user_id: current_user.id)
   end
-
 end
--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,12 +2,11 @@ class Habit < ApplicationRecord
   belongs_to :user
   has_many :articles
   def start_time
-    self.date
+    date
   end
 
   with_options presence: true do
     validates :date
     validates :achieve
   end
-
 end
