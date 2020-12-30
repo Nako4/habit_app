e6445dae2cb29d5aca6dafe20eab840b89eeea3f
Nako4 <papnoatelier@gmail.com>
2020-12-30 12:45:34 +0900

    feat: 同じ日に登録すると値が複数登録されたため、値を更新する機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update, :destroy]
 
-  def create
-    @article = Article.where(user_id: current_user.id)
-    @habit = Habit.create(habit_params)
-    redirect_to users_show_path
-  end
-
   def show
     @articles = Article.where(user_id: current_user.id).order('created_at DESC')
     @habits = Habit.where(user_id: current_user.id)
+  end
+
+  def create
+    @article = Article.where(user_id: current_user.id)
+    @habit = Habit.new(habit_params)
+    habit = Habit.where(date: habit_params[:date], user_id: habit_params[:user_id])
+    if (habit.present?)
+      habit.update(habit_params)
+    else
+      @habit.save
+    end
+    redirect_to users_show_path
   end
 
   private
@@ -17,4 +23,5 @@ class UsersController < ApplicationController
   def habit_params
     params.permit(:date, :achieve).merge(user_id: current_user.id)
   end
+  
 end
