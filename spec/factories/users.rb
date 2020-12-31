4f6e8570f9f6cc0526f0773963eacb42b2f8c189
Nako4 <papnoatelier@gmail.com>
2020-12-31 19:24:38 +0900

    test: ユーザーモデルの結合テストコードを記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     nickname { Faker::Name.initials(number: 2) }
     email { Faker::Internet.free_email }
     password { Faker::Internet.password(min_length: 6, max_length: 128) }
+    password_confirmation { password }
   end
 end
