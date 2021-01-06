4bcae5defa5f8a4ad1d09c9ce2e34d0d90e511e8
Nako4 <papnoatelier@gmail.com>
2021-01-06 19:04:42 +0900

    refactor: 必要のないアソシエーションがあったため、削除、修正

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,6 +1,5 @@
 class Habit < ApplicationRecord
   belongs_to :user
-  has_many :articles
   def start_time
     date
   end
--- db/migrate/20201128034607_create_habits.rb
+++ db/migrate/20201128034607_create_habits.rb
@@ -2,7 +2,7 @@ class CreateHabits < ActiveRecord::Migration[6.0]
   def change
     create_table :habits do |t|
       t.integer :user_id, foreign_key: true
-      t.date :date
+      t.date :date, null: false
       t.integer :achieve
 
       t.timestamps
