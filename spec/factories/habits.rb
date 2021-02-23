c38944a9a9abff2124b3fd7e114313e3cfe6c3db
Nako4 <papnoatelier@gmail.com>
2021-02-23 12:15:51 +0900

    Update habits.rb

--- spec/factories/habits.rb
+++ spec/factories/habits.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :habit do
-    date { Faker::date.backward }
+    date { Faker::date.in_date_period }
     achieve { 100 }
     user_id { FactoryBot.create(:user).id }
   end
