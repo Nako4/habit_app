f8aa8bde448ed404fe1dde2af0e32335ff995101
Nako4 <papnoatelier@gmail.com>
2021-02-23 12:31:41 +0900

    userモデルのpasswordのバリデーションは15文字までであったため、単体テストコードで生成されるパスワードを15文字に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials(number: 2) }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6, max_length: 128) }
+    password { Faker::Internet.password(min_length: 6, max_length: 15) }
     # password { SecureRandom.urlsafe_base64}
     # password { SecureRandom.alphanumeric}
     password_confirmation { password }
